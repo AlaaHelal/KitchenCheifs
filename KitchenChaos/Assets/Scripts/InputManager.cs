aad40252b91dd931f04856415b95aab19bd1f705
AlaaAllahHelal <alaa.helal98@gmail.com>
2026-08-05 09:56:24 +0300

    InteractAction
    Used Events to fires when player press E button to interact with counter

--- KitchenChaos/Assets/Scripts/InputManager.cs
+++ KitchenChaos/Assets/Scripts/InputManager.cs
@@ -1,42 +1,26 @@
+using System;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
     PlayerInputActions inputActions;
+    public event EventHandler OnInteractAction;
 
     private void Awake() {
         inputActions = new PlayerInputActions();
         inputActions.Player.Enable();
+
+        inputActions.Player.Interact.performed += Interact_performed;
     }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+       OnInteractAction?.Invoke(this, EventArgs.Empty);
+    }
+
     public Vector2 GetMovementVectorNormalized() {
 
         Vector2 inputVector = inputActions.Player.Move.ReadValue<Vector2>();
         return inputVector.normalized;
-
-        /*
-        // Must initialize inputVector to zero inside update to avoid the input from keeping going in the same direction
-        Vector2 inputVector = Vector2.zero;
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
-
-        inputVector = inputVector.normalized;
-
-        return inputVector;
-        */
     }
 }
--- KitchenChaos/Assets/Scripts/PlayerController.cs
+++ KitchenChaos/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,9 +8,34 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask interactLayerMask;
 
     private bool isWalking;
-    private Vector3 lastInteractDir;    
+    private Vector3 lastInteractDir;
 
-    
+    private void Start()
+    {
+        inputManager.OnInteractAction += InputManager_OnInteractAction;
+    }
+
+    private void InputManager_OnInteractAction(object sender, EventArgs e)
+    {
+        Vector2 inputVector = inputManager.GetMovementVectorNormalized();
+        Vector3 movDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if (movDir != Vector3.zero)
+        {
+            lastInteractDir = movDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, interactLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+
+                clearCounter.Interact();
+            }
+        }
+    }
+
     private void Update()
     {
         //Move the player 
@@ -26,32 +52,33 @@ public class PlayerController : MonoBehaviour
 
     private void HandleMovement()
     {
+        //Get the input vector and convert it to a 3D movement direction
         Vector2 inputVector = inputManager.GetMovementVectorNormalized();
-
         Vector3 movDir = new Vector3(inputVector.x, 0, inputVector.y);
 
+        //Check if the player can move in the desired direction using a capsule cast to detect collisions
         float playerRadius = 0.5f;
         float playerHeight = 2f;
-        float mavDistance = moveSpeed * Time.deltaTime;
+        float movDistance = moveSpeed * Time.deltaTime;
         Vector3 pointA = transform.position;
         Vector3 pointB = transform.position + Vector3.up * playerHeight;
-        bool canMove = !Physics.CapsuleCast(pointA, pointB, playerRadius, movDir, mavDistance);
+        bool canMove = !Physics.CapsuleCast(pointA, pointB, playerRadius, movDir, movDistance);
 
         if (!canMove)
         {
             //Cannot move towards movDir, attempt to move along individual axes (split movdir)
 
             //Attempt to move only along the X axis
-            Vector3 moveDirX = new Vector3(movDir.x, 0, 0);
-            canMove = !Physics.CapsuleCast(pointA, pointB, playerRadius, moveDirX, mavDistance);
+            Vector3 moveDirX = new Vector3(movDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(pointA, pointB, playerRadius, moveDirX, movDistance);
             if (canMove)
                 //Move only along X axis
                 movDir = moveDirX;
             else
             {
                 //Attempt to move only along the Z axis
-                Vector3 moveDirZ = new Vector3(0, 0, movDir.z);
-                canMove = !Physics.CapsuleCast(pointA, pointB, playerRadius, moveDirZ, mavDistance);
+                Vector3 moveDirZ = new Vector3(0, 0, movDir.z).normalized;
+                canMove = !Physics.CapsuleCast(pointA, pointB, playerRadius, moveDirZ, movDistance);
                 if (canMove)
                     //Move only along Z axis
                     movDir = moveDirZ;
@@ -62,15 +89,17 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        //Move the player if possible
         if (canMove)
         {
             transform.position += movDir * moveSpeed * Time.deltaTime;
         }
+
         // Smoothly rotate towards movement direction
         float rotationSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, movDir, rotationSpeed * Time.deltaTime);
 
-
+        // Update the walking state
         isWalking = movDir != Vector3.zero;
     }
 
@@ -78,6 +107,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 inputVector = inputManager.GetMovementVectorNormalized();
         Vector3 movDir = new Vector3(inputVector.x, 0, inputVector.y);
+
         if (movDir != Vector3.zero)
         {
             lastInteractDir = movDir;
@@ -88,16 +118,9 @@ public class PlayerController : MonoBehaviour
         {
             if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
 
-                clearCounter.Interact();
+                //clearCounter.Interact();
             }
         }
-        else
-        {
-            Debug.Log("Nothing hit");
-        }
-
-
-
     }
         
 }
